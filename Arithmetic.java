c20b042afba620c7967490aae482eb812c008259
Lakshmi-Bhavani <84325499+Lakshmi-Bhavani@users.noreply.github.com>
2021-08-23 13:10:24 +0530

    Add files via upload

---
+++ Arithmetic.java
@@ -0,0 +1,19 @@
+public class Arithmetic
+{
+public static void main(String[] args)
+{
+int a=20;
+int b=10;
+int c=0;
+int d=20;
+int e=40;
+int f=30;
+String x= "welcome", y="java";
+System.out.println("a+b=" +(a+b));
+System.out.println("a-b=" +(a-b));
+System.out.println("x+y=" +(x+y));
+System.out.println("a*b=" +(a*b));
+System.out.println("a/b=" +(a/b));
+System.out.println("a%b=" +(a%b));
+}
+} 
